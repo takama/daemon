a2b20e30d447a7916ca69ed4b7104db22e7aa561
Igor Dolzhikov <bluesriverz@gmail.com>
2014-08-07 03:15:16 +0700

    move helper methods outside

--- daemon.go
+++ daemon.go
@@ -143,19 +143,6 @@ Go daemon
 */
 package daemon
 
-import (
-	"os"
-	"os/exec"
-	"os/user"
-)
-
-// Service constants
-const (
-	rootPrivileges = "You must have root user privileges. Possibly using 'sudo' command should help"
-	success        = "\t\t\t\t\t[  \033[32mOK\033[0m  ]" // Show colored "OK"
-	failed         = "\t\t\t\t\t[\033[31mFAILED\033[0m]" // Show colored "FAILED"
-)
-
 // Daemon interface has standard set of a methods/commands
 type Daemon interface {
 
@@ -181,25 +168,4 @@ type Daemon interface {
 // description: any explanation, what is the service, its purpose
 func New(name, description string) (Daemon, error) {
 	return newDaemon(name, description)
-}
-
-// Lookup path for executable file
-func executablePath(name string) (string, error) {
-	if path, err := exec.LookPath(name); err == nil {
-		_, err := os.Stat(path)
-		if os.IsNotExist(err) {
-			return execPath()
-		}
-		return path, nil
-	}
-	return execPath()
-}
-
-// Check root rights to use system service
-func checkPrivileges() bool {
-
-	if user, err := user.Current(); err == nil && user.Gid == "0" {
-		return true
-	}
-	return false
 }
