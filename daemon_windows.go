1fe5c082129afbf41749ea1a145722b558359338
Fatih Kaya <1994274@gmail.com>
2016-05-28 08:48:25 +0000

    Add windows support.
    Signed-off-by: Fatih Kaya <1994274@gmail.com>

--- daemon_windows.go
+++ daemon_windows.go
@@ -6,10 +6,12 @@
 package daemon
 
 import (
+	"os/exec"
 	"errors"
+	"github.com/kardianos/osext"
 )
 
-var ErrWindowsUnsupported = errors.New("windows daemon is not supported")
+var ErrWindowsUnsupported = errors.New("Adding as a service failed. Download and place nssm.exe in the path to install this service as an service. NSSM url: https://nssm.cc/")
 
 // windowsRecord - standard record (struct) for windows version of daemon package
 type windowsRecord struct {
@@ -27,37 +29,66 @@ func newDaemon(name, description string, dependencies []string) (Daemon, error) 
 func (windows *windowsRecord) Install(args ...string) (string, error) {
 	installAction := "Install " + windows.description + ":"
 
-	return installAction + failed, ErrWindowsUnsupported
+	execp, err := execPath()
+
+	if err != nil {
+		return installAction + failed, err
+	}
+
+	cmd := exec.Command("nssm.exe install Description=\"" + windows.description + "\" \"" + windows.name + "\" " + execp)
+	err = cmd.Run()
+	if err != nil {
+		return installAction + failed, err
+	}
+
+	return installAction + " completed.", nil
 }
 
 // Remove the service
 func (windows *windowsRecord) Remove() (string, error) {
 	removeAction := "Removing " + windows.description + ":"
-
-	return removeAction + failed, ErrWindowsUnsupported
+	cmd := exec.Command("nssm.exe remove \"" + windows.name + "\"")
+	err := cmd.Run()
+	if err != nil {
+		return removeAction + failed, err
+	}
+	return removeAction + " completed.", nil
 }
 
 // Start the service
 func (windows *windowsRecord) Start() (string, error) {
 	startAction := "Starting " + windows.description + ":"
-
+	cmd := exec.Command("nssm.exe start \"" + windows.name + "\"")
+	err := cmd.Run()
+	if err != nil {
+		return startAction + failed, err
+	}
 	return startAction + failed, ErrWindowsUnsupported
 }
 
 // Stop the service
 func (windows *windowsRecord) Stop() (string, error) {
 	stopAction := "Stopping " + windows.description + ":"
+	cmd := exec.Command("nssm.exe stop \"" + windows.name + "\"")
+	err := cmd.Run()
+	if err != nil {
+		return stopAction + failed, err
+	}
 
 	return stopAction + failed, ErrWindowsUnsupported
 }
 
 // Status - Get service status
 func (windows *windowsRecord) Status() (string, error) {
-
-	return "Status could not defined", ErrWindowsUnsupported
+	cmd := exec.Command("nssm.exe status \"" + windows.name + "\"")
+	out, err := cmd.Output()
+	if err != nil {
+		return stopAction + failed, err
+	}
+	return "Status: " + string(out), nil
 }
 
 // Get executable path
 func execPath() (string, error) {
-	return "", ErrWindowsUnsupported
+	return osext.Executable()
 }
