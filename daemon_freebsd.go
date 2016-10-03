19cd2c8c487680e924109a3e3fd3b353f1d2e796
Erkan Durmus <derkan@gmail.com>
2016-10-03 10:23:42 +0300

    freebsd support

--- daemon_freebsd.go
+++ daemon_freebsd.go
@@ -14,6 +14,7 @@ import (
 	"path/filepath"
 )
 
+
 // systemVRecord - standard record (struct) for linux systemV version of daemon package
 type bsdRecord struct {
 	name         string
@@ -35,6 +36,44 @@ func (bsd *bsdRecord) isInstalled() bool {
 
 	return false
 }
+
+
+// Get the daemon properly
+func newDaemon(name, description string, dependencies []string) (Daemon, error) {
+	return &bsdRecord{name, description, dependencies}, nil
+}
+
+func execPath() (string, error) {
+	PATH_MAX := 1024 // From <sys/syslimits.h>
+	exePath := make([]byte, PATH_MAX)
+	exeLen := C.size_t(len(exePath))
+
+	// Beware: sizeof(int) != sizeof(C.int)
+	var mib [4]C.int
+	// From <sys/sysctl.h>
+	mib[0] = 1  // CTL_KERN
+	mib[1] = 14 // KERN_PROC
+	mib[2] = 12 // KERN_PROC_PATHNAME
+	mib[3] = -1
+
+	status, err := C.sysctl((*C.int)(unsafe.Pointer(&mib[0])), 4, unsafe.Pointer(&exePath[0]), &exeLen, nil, 0)
+
+	if err != nil {
+		return "", fmt.Errorf("sysctl: %v", err)
+	}
+
+	// Not sure why this might happen with err being nil, but...
+	if status != 0 {
+		return "", fmt.Errorf("sysctl returned %d", status)
+	}
+
+	// Convert from null-padded []byte to a clean string. (Can't simply cast!)
+	exePathStringLen := bytes.Index(exePath, []byte{0})
+	exePathString := string(exePath[:exePathStringLen])
+
+	return filepath.Clean(exePathString), nil
+}
+
 
 // Check service is running
 func (bsd *bsdRecord) checkRunning() (string, bool) {
