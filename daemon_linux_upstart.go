c6b487a2fa48330d19eb2f881a233181fce167b0
0X8C <nevermakeyoucrytoli@gmail.com>
2017-09-05 19:48:09 +0800

    Update daemon_linux_upstart.go

--- daemon_linux_upstart.go
+++ daemon_linux_upstart.go
@@ -186,6 +186,7 @@ author          "Pichu Chen <pichu@tih.tw>"
 start on runlevel [2345]
 stop on runlevel [016]
 
+respawn
 #kill timeout 5
 
 exec {{.Path}} {{.Args}} >> /var/log/{{.Name}}.log 2>> /var/log/{{.Name}}.err
